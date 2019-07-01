444ee6216cfdd40842756753ea65a5269d159bf1
kkabout <964902002@qq.com>
2019-07-01 22:33:45 +0800

    init

---
+++ src/main/java/com/wwdstore/wwd/WwdApplication.java
@@ -0,0 +1,13 @@
+package com.wwdstore.wwd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WwdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WwdApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wwdstore/wwd/WwdApplicationTests.java
@@ -0,0 +1,16 @@
+package com.wwdstore.wwd;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WwdApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
